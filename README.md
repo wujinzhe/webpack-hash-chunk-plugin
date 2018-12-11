89ad0d47bdcc5d125f90a8f6663009e741d55ef0
吴晋哲 <wujinzhe@iboxpay.com>
2018-12-11 16:36:35 +0800

    完成初始版本

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # webpack-hash-chunk-plugin
-webpack 将chunkid设置为hash值
+将打包出来的chunkId使用hash值代替webpack原本的id值
+
+##### 安装
+
+``` npm i webpack-hash-chunk-plugin -D ```
+
+##### 使用
+
+``` JS
+
+const WebpackHashChunkPlugin = require('webpack-hash-chunk-plugin')
+
+module.exports = {
+  plugins: [
+    new WebpackHashChunkPlugin({
+      algorithm: 'md5',  // 哈希算法  md5,sha,sha1,sha256 等
+      encoding: 'hex',   // hash编码的方法 只有hex, latin1, base64三个选项
+      length: 4 // 生成的hash值的长度  长度值为1~32
+    })
+  ]
+}
+
+```
+
+![结果](./image/1.png)
---
+++ index.js
@@ -0,0 +1,28 @@
+const createHash = require('crypto').createHash
+class WebpackHashChunkPlugin {
+  constructor(options) {
+    this.options = Object.assign({
+      algorithm: 'md5',
+      encoding: 'hex',   // hex, latin1, base64
+      length: 4
+    }, options)
+  }
+
+  apply(compiler) {
+    const options = this.options;
+    compiler.hooks.compilation.tap('WebpackHashChunkPlugin', (compilation) => {
+      compilation.hooks.afterOptimizeChunkIds.tap('WebpackHashChunkPlugin', (chunks) => {
+        chunks.forEach(item => {
+          const hash = createHash(options.algorithm)
+          hash.update(`${new Date().getTime()}${item.id}`)
+          let hashId = hash.digest(options.encoding)
+          // hashId = hashId.substr(0, options.length)
+          item.id = hashId
+          item.ids = [hashId]
+        })
+      })
+    })
+  }
+}
+
+module.exports = WebpackHashChunkPlugin;
