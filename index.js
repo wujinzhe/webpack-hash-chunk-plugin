c3352911a51e9b69c4ffddd77b5dfb09c219790a
吴晋哲 <wujinzhe@iboxpay.com>
2018-12-15 14:38:20 +0800

    根据路径和ID产生hash

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class WebpackHashChunkPlugin {
       compilation.hooks.afterOptimizeChunkIds.tap('WebpackHashChunkPlugin', (chunks) => {
         chunks.forEach(item => {
           const hash = createHash(options.algorithm)
-          hash.update(`${__dirname}${item.id}`)
+          hash.update(`${process.env.PWD}${item.id}`)
           let hashId = hash.digest(options.encoding)
           hashId = hashId.substr(0, options.length)
           item.id = hashId
